bd794d5b7bede59de59187eeeb6954804ec1e98b
goeuddeum <125548154+goeuddeum@users.noreply.github.com>
2023-05-01 12:14:16 +0900

    Add files via upload

---
+++ 논리연산.py
@@ -0,0 +1,14 @@
+Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 3>4 and 5>6
+False
+>>> 3<4 and 5<6
+True
+>>> 3>4 or 5>6
+False
+>>> 3<4 or 5>6
+True
+>>> not 3<4
+False
+>>> not 3>4
+True
